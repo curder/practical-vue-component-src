5f047c79a9c51447b78c1d2e955224fb4abf75e1
curder <curder@foxmail.com>
2019-07-01 11:49:53 +0800

    添加一个向下滚动的指示svg

--- src/views/NumberCount.vue
+++ src/views/NumberCount.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <h2 class="text-center text-2xl text-gray-700 pt-8">请往下滑动鼠标查看滚动的数字效果</h2>
+    <h2 class="text-center text-2xl text-gray-600 pt-8">请往下滑动鼠标查看滚动的数字效果</h2>
+    <div class="flex items-center justify-center bounce">
+      <svg
+        class="fill-current text-gray-500 w-12 h-24"
+        viewBox="0 0 1024 1024"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M483.555556 853.333333a28.558222 28.558222 0 0 0 20.138666-8.305777L745.073778 603.648a28.444444 28.444444 0 0 0-40.220445-40.220444L483.555556 784.668444 262.314667 563.427556A28.444444 28.444444 0 0 0 222.094222 603.648l241.322667 241.379556a28.558222 28.558222 0 0 0 20.138667 8.305777z"
+        ></path>
+        <path
+          d="M455.111111 824.888889V199.111111a28.444444 28.444444 0 0 1 56.888889 0v625.777778a28.444444 28.444444 0 0 1-56.888889 0z"
+        ></path>
+      </svg>
+    </div>
     <div
       class="flex flex-col xl:flex-row items-center justify-center min-h-screen bg-gray-100"
       style="padding-top: 1000px; padding-bottom: 500px;"
@@ -41,3 +55,24 @@ export default {
   }
 };
 </script>
+<style>
+.bounce {
+  animation: bounce 2s infinite;
+}
+
+@keyframes bounce {
+  0%,
+  20%,
+  50%,
+  80%,
+  100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-30px);
+  }
+  60% {
+    transform: translateY(-15px);
+  }
+}
+</style>
