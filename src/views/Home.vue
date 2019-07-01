1637ff7954730d5b5769319ff80a60192c0d8f07
curder <curder@foxmail.com>
2019-07-01 16:47:26 +0800

    首页文字大小和文字颜色调整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex justify-center items-center">
     <div class="text-center">
-      <h1 class="text-6xl text-gray-800 font-bold">实用的Vue组件</h1>
-      <p class="text-xl text-gray-600 tracking-wider uppercase font-bold">总结一些常见的Web实用Vue组件</p>
+      <h1 class="text-5xl text-gray-700 font-bold">实用的Vue组件</h1>
+      <p class="text-xl text-gray-500 tracking-wider uppercase font-bold">总结一些常见的Web实用Vue组件</p>
     </div>
   </div>
 </template>
