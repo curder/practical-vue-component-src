db23ba3f09b4c3e36ec066720f0d2b25c6ae9633
curder <curder@users.noreply.github.com>
2022-03-07 03:24:53 +0000

    deploy: b2679437df67a3240c95e4051a0f740636f0e4e5

--- src/views/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex justify-center items-center">
-    <div class="text-center">
-      <h1 class="text-5xl text-gray-700 font-bold">实用的Vue组件</h1>
-      <p class="text-xl text-gray-500 tracking-wider uppercase font-bold">总结一些常见的Web实用Vue组件</p>
-    </div>
-  </div>
-</template>
